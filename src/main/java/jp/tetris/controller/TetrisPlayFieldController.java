dfa4bd37cae512d5f4307c2f15086d3cacc19907
kitajima <kitajima@m2j.co.jp>
2016-06-22 20:36:46 +0900

    タイムライン処理の追加
    キー操作落下のみ追加

--- src/main/java/jp/tetris/controller/TetrisPlayFieldController.java
+++ src/main/java/jp/tetris/controller/TetrisPlayFieldController.java
@@ -2,14 +2,19 @@ package jp.tetris.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import jp.tetris.tetorimino.RandomTetoriminoBuilder;
 import jp.tetris.tetorimino.Tetorimino;
-
 
 /**
  * フィールド フィールド内のテトリミノを管理
@@ -28,13 +33,15 @@ public class TetrisPlayFieldController implements Initializable {
 	// ブロックサイズ
 	private final int BLOCK_SIZE = 15;
 	private final RandomTetoriminoBuilder RandomTetoriminoBuilder = new RandomTetoriminoBuilder();
+	//タイムライン
+	private Timeline timeLine;
 
 	@Override
 	public void initialize(final URL location, final ResourceBundle resources) {
 
 		this.fieldPanel.setFocusTraversable(true);
 		this.fieldPanel.requestFocus();
-	
+		operationKeySetting();
 		initField();
 
 	}
@@ -52,6 +59,10 @@ public class TetrisPlayFieldController implements Initializable {
 				this.fieldPanel.add(fieldBlock, j, i);
 			}
 		}
+		/**
+		 *定期処理 
+		 */
+		timeLine();
 	}
 
 	/**
@@ -60,18 +71,51 @@ public class TetrisPlayFieldController implements Initializable {
 	public void drawTetorimino() {
 
 		// TODO ランダムにテトリミノを呼び出し
-		Tetorimino tetorimino = this.RandomTetoriminoBuilder.createTetoriminoShape();
-		
+		Tetorimino tetorimino = this.RandomTetoriminoBuilder
+				.createTetoriminoShape();
+
 		for (int i = 0; i < Tetorimino.getSIZE(); i++) {
 			for (int j = 0; j < Tetorimino.getSIZE(); j++) {
-				Rectangle tetoriminoBlock = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
-				tetoriminoBlock.setFill(Tetorimino.getFillColor(tetorimino.getShape().get(0)[i][j]));
+				Rectangle tetoriminoBlock = new Rectangle(BLOCK_SIZE,
+						BLOCK_SIZE);
+				tetoriminoBlock.setFill(Tetorimino.getFillColor(tetorimino
+						.getShape().get(0)[i][j]));
 				this.baPanel.add(tetoriminoBlock, j, i);
 
 			}
 		}
 	}
 
-	
+	/**
+	 * キー操作設定
+	 */
+	private void operationKeySetting() {
+		this.fieldPanel.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent keyEvent) {
+
+				if (keyEvent.getCode() == KeyCode.DOWN) {
+					System.out.println("key_down");
+					fallTetorimino();
+					keyEvent.consume();
+				}
+			}
+		});
+	}
+	/**
+	 * 定期処理登録
+	 */
+	private void timeLine() {
+		timeLine = new Timeline(new KeyFrame(Duration.millis(500),
+				ae -> fallTetorimino()));
+		timeLine.setCycleCount(Timeline.INDEFINITE);
+		timeLine.play();
+	}
+
+	// TODO キー操作落下
+	private void fallTetorimino() {
+		
+		System.out.println("key_down");
+	}
 
 }
