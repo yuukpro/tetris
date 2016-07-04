0942deb03920e4b1d85d7b1251609aba06c0ea38
kitajimayuu <yuuk1990cs@gmail.com>
2016-07-04 09:20:58 +0900

    列の削除処理を追加

--- src/main/java/jp/tetris/controller/TetrisPlayFieldController.java
+++ src/main/java/jp/tetris/controller/TetrisPlayFieldController.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -35,6 +34,8 @@ public class TetrisPlayFieldController implements Initializable {
 	// ブロックサイズ
 	private final int BLOCK_SIZE = 15;
 
+	// フィールド上のテトリミノ状態管理
+	private int[][] blockField = new int[this.FIELD_HEIGHT][this.FIELD_WIDTH];
 	// タイムライン
 	private Timeline timeLine;
 	private int tetoriminoNo = 0;
@@ -65,9 +66,27 @@ public class TetrisPlayFieldController implements Initializable {
 				Rectangle fieldBlock = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
 				fieldBlock.setFill(Color.TRANSPARENT);
 				this.fieldPanel.add(fieldBlock, j, i);
+				this.blockField[i][j] = 0;
 
 			}
 		}
+	}
+
+	private void resetField() {
+		// テトリスフィールドの大きさ初期化
+		for (int i = 0; i < this.FIELD_HEIGHT; i++) {
+			for (int j = 0; j < this.FIELD_WIDTH; j++) {
+				Rectangle fieldBlock = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
+				// フィールド上にテトリミノが配置されている場合は色をつける
+				if (this.blockField[i][j] > 0) {
+					fieldBlock.setFill(Tetorimino.getFillColor(this.blockField[i][j]));
+				} else {
+					fieldBlock.setFill(Color.TRANSPARENT);
+				}
+				this.fieldPanel.add(fieldBlock, j, i);
+			}
+		}
+
 	}
 
 	/**
@@ -75,18 +94,16 @@ public class TetrisPlayFieldController implements Initializable {
 	 */
 	private void drawTetorimino() {
 
-		for (String key : this.tetoriminoMap.keySet()) {
-			for (int i = 0; i < Tetorimino.getSIZE(); i++) {
-				for (int j = 0; j < this.tetoriminoMap.get(key).getShape().get(0)[i].length; j++) {
-					Rectangle tetoriminoBlock = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
-					tetoriminoBlock
-							.setFill(Tetorimino.getFillColor(this.tetoriminoMap.get(key).getShape().get(0)[i][j]));
-					this.fieldPanel.add(tetoriminoBlock, j + this.tetoriminoMap.get(key).getPositionX(),
-							i + this.tetoriminoMap.get(key).getPositionY());
-
-				}
+		for (int i = 0; i < Tetorimino.getSIZE(); i++) {
+			for (int j = 0; j < this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getShape()
+					.get(0)[i].length; j++) {
+				Rectangle tetoriminoBlock = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
+				tetoriminoBlock.setFill(Tetorimino.getFillColor(
+						this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getShape().get(0)[i][j]));
+				this.fieldPanel.add(tetoriminoBlock,
+						j + this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX(),
+						i + this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionY());
 			}
-
 		}
 	}
 
@@ -94,6 +111,7 @@ public class TetrisPlayFieldController implements Initializable {
 	 * テトリミノ登録
 	 */
 	public void entryTetorimino() {
+
 		this.tetoriminoNo++;
 		RandomTetoriminoGenerator RandomTetoriminoGenerator = new RandomTetoriminoGenerator();
 		this.tetoriminoMap.put(String.valueOf(this.tetoriminoNo), RandomTetoriminoGenerator.createTetoriminoShape());
@@ -107,25 +125,21 @@ public class TetrisPlayFieldController implements Initializable {
 			@Override
 			public void handle(KeyEvent keyEvent) {
 				if (keyEvent.getCode() == KeyCode.DOWN) {
-					System.out.println("key_down");
 					fallTetorimino();
 					keyEvent.consume();
 				}
 
-				if (keyEvent.getCode() == KeyCode.LEFT ) {
-					System.out.println("左");
+				if (keyEvent.getCode() == KeyCode.LEFT) {
 					reftMoveTetorimino();
 
 				}
-				if (keyEvent.getCode() == KeyCode.RIGHT ) {
+				if (keyEvent.getCode() == KeyCode.RIGHT) {
 					rightMoveTetoriminoe();
-					System.out.println("右");
 
 				}
 				if (keyEvent.getCode() == KeyCode.UP || keyEvent.getCode() == KeyCode.W) {
-					System.out.println("上");
 					rotation();
-				
+
 				}
 			}
 
@@ -147,6 +161,7 @@ public class TetrisPlayFieldController implements Initializable {
 		 */
 		if (this.fall.isFall(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)))) {
 			this.updateFallposition(String.valueOf(this.tetoriminoNo));
+			this.updateField();
 			this.entryTetorimino();
 			this.fall.reset();
 		} else {
@@ -154,9 +169,49 @@ public class TetrisPlayFieldController implements Initializable {
 		}
 	}
 
-	private void updateFallposition(String key) {
+	private void updateField() {
+		int deleteBlock = 0;
+		int matchCount = 0;
 		for (int i = 0; i < Tetorimino.getSIZE(); i++) {
-			for (int j = 0; j <this.tetoriminoMap.get(key).getShape().get(0)[i].length ; j++) {
+			for (int j = 0; j < this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getShape()
+					.get(0)[i].length; j++) {
+				if (this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getShape().get(0)[i][j] > 0) {
+					this.blockField[i + this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionY()][j
+							+ this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo))
+									.getPositionX()] = this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo))
+											.getShape().get(0)[i][j];
+				}
+
+			}
+		}
+
+		for (int i = 0; i < this.FIELD_HEIGHT; i++) {
+			matchCount = 0;
+			System.out.println();
+			for (int j = 0; j < this.FIELD_WIDTH; j++) {
+				System.out.print(this.blockField[i][j]);
+				if (this.blockField[i][j] > 0) {
+					matchCount++;
+					this.blockField[i][j] = this.blockField[i + deleteBlock][j];
+				}
+				if (this.FIELD_WIDTH == matchCount) {
+					deleteRow(i);
+					deleteBlock++;
+				}
+			}
+		}
+	}
+
+	private void deleteRow(final int i) {
+		for (int j = 0; j < this.FIELD_WIDTH; j++) {
+			this.blockField[i][j] = 0;
+		}
+
+	}
+
+	private void updateFallposition(final String key) {
+		for (int i = 0; i < Tetorimino.getSIZE(); i++) {
+			for (int j = 0; j < this.tetoriminoMap.get(key).getShape().get(0)[i].length; j++) {
 				if (this.tetoriminoMap.get(key).getShape().get(0)[i][j] > 0) {
 					this.fall.updatePositionMap(j + this.tetoriminoMap.get(key).getPositionX(),
 							i + this.tetoriminoMap.get(key).getPositionY());
@@ -167,29 +222,41 @@ public class TetrisPlayFieldController implements Initializable {
 
 	// テトリミノ落下処理
 	private void fallTetorimino() {
+
 		// 落下判定
 		this.fallCheck();
 		this.fieldPanel.getChildren().clear();
-		this.initField();
-		
-			// 操作中テトリミノの座標を一段下げる
-			this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).setPositionY(this.fall.getFALL_COUNT());
-		
+		this.resetField();
+		// 操作中テトリミノの座標を一段下げる
+		this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).setPositionY(this.fall.getFALL_COUNT());
+
 		// 登録テトリミノをフィールドに表示
 		drawTetorimino();
 	}
-	private void rightMoveTetoriminoe(){
-		if(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX()<=this.FIELD_WIDTH){
-		this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).setPositionX(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX()+1);
+
+	/***
+	 * テトリミノ右移動
+	 */
+	private void rightMoveTetoriminoe() {
+		if (this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX() + this.tetoriminoMap
+				.get(String.valueOf(this.tetoriminoNo)).getShape().get(0)[1].length <= this.FIELD_WIDTH - 1) {
+			this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo))
+					.setPositionX(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX() + 1);
 		}
 	}
-	private void reftMoveTetorimino(){
-		if(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX()!=0){
-		this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).setPositionX(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX()-1);
-	
+
+	/***
+	 * テトリミノ左移動
+	 */
+	private void reftMoveTetorimino() {
+		if (this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX() != 0) {
+			this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo))
+					.setPositionX(this.tetoriminoMap.get(String.valueOf(this.tetoriminoNo)).getPositionX() - 1);
+
 		}
 	}
-	
+
+	// TODO 回転処理
 	public void rotation() {
 		this.tetoriminoMap.get(this.tetoriminoNo).Rotation();
 	}
